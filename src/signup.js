21cb25d1ba6c4fe91e9e3df2a10d7f00d98e1f0a
Raj Trivedi <triudrav@udel.edu>
2021-09-25 21:56:29 -0400

    Minor fix in email checker logic

--- src/signup.js
+++ src/signup.js
@@ -28,8 +28,8 @@ document.getElementById("signup-form").onsubmit = function(){
             return false;
         }
         else{
-            if(!key.endsWith(".org") && !key.endsWith(".com") && !key.endsWith(".edu") ){
-                document.getElementById("emailChecker").innerText = "Invalid email extension(use .org, .edu, or .com)";
+            if(!key.endsWith(".org") && !key.endsWith(".com") && !key.endsWith(".edu") && !key.endsWith(".in") && !key.endsWith(".co.in") ){
+                document.getElementById("emailChecker").innerText = "Invalid email extension(use .org, .edu, .in, .co.in or .com)";
                 document.getElementById("emailChecker").style = "color: red"; 
                 return false;
             }
@@ -41,8 +41,8 @@ document.getElementById("signup-form").onsubmit = function(){
         }
     }
     else{
-        if(!key.endsWith(".org") && !key.endsWith(".com") && !key.endsWith(".edu") ){
-            document.getElementById("emailChecker").innerText = "Invalid email extension(use .org, .edu, or .com)";
+        if(!key.endsWith(".org") && !key.endsWith(".com") && !key.endsWith(".edu") && !key.endsWith(".in") && !key.endsWith(".co.in") ){
+            document.getElementById("emailChecker").innerText = "Invalid email extension(use .org, .edu, .in, .co.in or .com)";
             document.getElementById("emailChecker").style = "color: red"; 
             return false;
         }

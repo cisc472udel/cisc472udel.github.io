111ef82d3309c0b3ce16bbe7dcf4b6899e62c1fa
davidlizotte <47873116+davidlizotte@users.noreply.github.com>
2021-10-17 23:39:02 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -167,12 +167,12 @@ let serverClickHandler = function(name, username, useremail){
         let message1 = document.getElementById("message-field").value;
         document.getElementById("message-field").value="";
         const currenttime = new Date();
-        let timedisplay=currenttime.toUTCString();
+        
         
         
         let chatObj = { 
             "message": message1,
-            "timestamp": timedisplay,
+            "timestamp": currenttime.toUTCString(),
             "username": username
         };
        
